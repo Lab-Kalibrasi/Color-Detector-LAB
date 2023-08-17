cd983f72dcf28ec1274e88a7c7a5a2075c8f4fee
Rizkie Pratama <itsme@rizkiepratama.net>
2023-08-18 02:41:49 +0700

    Upgrade to Latest Kotlin, SDK and Gradle Version

--- app/src/main/java/com/doctorblue/colordetector/dialog/ColorDetailDialog.kt
+++ app/src/main/java/com/doctorblue/colordetector/dialog/ColorDetailDialog.kt
@@ -4,9 +4,12 @@ import android.app.Dialog
 import android.content.Context
 import android.graphics.Color
 import android.os.Bundle
+import android.view.View
+import android.widget.RelativeLayout
+import android.widget.TextView
 import com.doctorblue.colordetector.R
 import com.doctorblue.colordetector.model.UserColor
-import kotlinx.android.synthetic.main.dialog_color_detail.*
+import com.google.android.material.button.MaterialButton
 
 class ColorDetailDialog(
     context: Context,
@@ -14,10 +17,24 @@ class ColorDetailDialog(
     private val onRemove: (UserColor) -> Unit
 ) : Dialog(context) {
 
+    private lateinit var view_color_preview: View;
+    private lateinit var txt_rgb: TextView;
+    private lateinit var txt_hex: TextView;
+    private lateinit var txt_hsl: TextView;
+    private lateinit var btn_cancel: MaterialButton;
+    private lateinit var btn_remove_color: MaterialButton;
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.dialog_color_detail)
         setTitle(context.resources.getString(R.string.your_color))
+
+        view_color_preview = findViewById(R.id.view_color_preview)
+        txt_rgb = findViewById(R.id.txt_rgb)
+        txt_hex = findViewById(R.id.txt_hex)
+        txt_hsl = findViewById(R.id.txt_hsl)
+        btn_cancel = findViewById(R.id.btn_cancel)
+        btn_remove_color = findViewById(R.id.btn_remove_color)
 
         view_color_preview.setBackgroundColor(Color.parseColor(color.hex))
 
